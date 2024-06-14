d44a32c34ab3ec73ad0062b61685446dbe98fe9a
Filip Åhfelt <dev@filip.mozmail.com>
2024-06-14 10:47:20 +0200

    update html backdrop

--- src/components/html-backdrop.tsx
+++ src/components/html-backdrop.tsx
@@ -1,7 +1,7 @@
 export const HtmlBackdrop = () => {
   return (
     <section className="select-none text-center m-0 p-0 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 ">
-      <h1 className="leading-none text-[calc(30px+10vw)] ">k-day</h1>
+      <h1 className="leading-none text-[calc(30px+10vw)] ">k-lunch</h1>
       <small className="leading-none text-[calc(12px+0.71625vw)]">
         2024 malmö
       </small>
